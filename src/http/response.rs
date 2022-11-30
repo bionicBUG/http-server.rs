1ae3daedc58814f1bcb5b34332322bda863f66a2
Ojas Sinha <sinhaojas67@gmail.com>
2022-12-01 03:48:55 +0530

    init http server
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

---
+++ src/http/response.rs
@@ -0,0 +1,29 @@
+use std::fmt::{Display, Result as FmtResult};
+use std::io::{Result as IOResult, Write};
+use std::net::TcpStream;
+
+use super::StatusCode;
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Response { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut TcpStream) -> IOResult<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body
+        )
+    }
+}
---
+++ src/website_handler.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+use crate::{
+    http::{Method, Request, Response, StatusCode},
+    server::Handler,
+};
+
+pub struct WebSiteHandler {
+    public_path: String,
+}
+
+impl WebSiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    pub fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    print!("Restricted access: {}", file_path);
+                    None
+                }
+            }
+
+            Err(_) => None
+        }
+    }
+}
+
+impl Handler for WebSiteHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1> Welcome </h1>".to_string())),
+                path => match self.read_file(path) {
+                    Some(content) => Response::new(StatusCode::Ok, Some(content)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
+            },
+            
+            _ => Response::new(StatusCode::NotFound, None),
+        }
+    }
+}
